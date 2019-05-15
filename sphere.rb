57c4c2d7d8cda4c745c39e6b84e318a3734c849b
albertshevchuk <albertshevchuk@gmail.com>
2019-05-14 21:40:36 -0700

    inheritance

--- sphere.rb
+++ sphere.rb
@@ -1,20 +1,30 @@
 class Sphere
-
 	def initialize(x)
 		@radius = x.to_f
 	end
 	
 	def area
-		return ((4*(@radius**2))*Math::PI)
+		((4*(@radius**2))*Math::PI)
 	end
 	
 	def volume
-		return ((4*(@radius**3))*(Math::PI/3))
+		((4*(@radius**3))*(Math::PI/3))
 	end
-
 end
 
-nw = Sphere.new 5
-puts nw.area
-puts nw.volume
+class Ball < Sphere
+	@@color = "Yellow"
+end
 
+class MyBall < Ball
+	@@owner = "Albert"
+	def show
+		print "The owner of the ball is " + @@owner + "\n"
+		print "The color of the ball is " + @@color + "\n"
+		print "The area of " + {Sphere:area}.to_s + "\n"
+		print "The area of " + {Sphere:volume}.to_s
+	end
+end
+
+nw = MyBall.new 5
+nw.show
